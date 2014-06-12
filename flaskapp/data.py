6e2abb096ce91d440febc7dd60864442d5f84358
sterutkb <stewart.rutledge@kb.se>
2014-06-12 09:24:46 +0200

    Adding support for sqlite

--- flaskapp/data.py
+++ flaskapp/data.py
@@ -6,13 +6,118 @@ if __name__ != '__main__':
     syspath.append(path.dirname(__file__))
     chdir(path.dirname(__file__))
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from sqlite3 import connect as sqconnect
 from yaml import load as yload
 from autobp import auto_bp, datasetRoot
 from os import path
 
+
 app = Flask(__name__)
 app.register_blueprint(auto_bp, url_prefix='/datasets')
+gconn = sqconnect('./data.db')
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+gconn.row_factory = dict_factory
+
+
+def _create_db():
+    lconn = sqconnect('./data.db')
+    cur = lconn.cursor()
+    cur.execute('''
+        CREATE TABLE
+        datasets(
+        datasetID INT PRIMARY KEY,
+        type TEXT,
+        name TEXT,
+        description TEXT,
+        license TEXT,
+        PATH text)
+    ''')
+    lconn.commit()
+    cur.execute('''
+        CREATE TABLE
+        sameAs(
+        sameAsID INT PRIMARY KEY,
+        datasetID INT,
+        librisid TEXT)
+    ''')
+    lconn.commit()
+    cur.execute('''
+        CREATE TABLE
+        distribution(
+        distID INT PRIMARY KEY,
+        datasetID INT,
+        encodingFormatID INT)
+    ''')
+    lconn.commit()
+    cur.execute('''
+        CREATE TABLE
+        providor(
+        prodvidorID, INT PRIMARY KEY
+        datasetID INT,
+        name TEXT,
+        email TEXT)
+    ''')
+    lconn.commit()
+    cur.close()
+
+try:
+    _create_db()
+except Exception as e:
+    print("Could not create database: %s" % e)
+
+
+def create_dataset(formdata):
+#    try:
+        conn = sqconnect('./data.db')
+        librisIDs = formdata['librisIDs'].split(',')
+        encodingFormats = formdata['encodingFormats'].split(',')
+        cur = conn.cursor()
+        cur.execute('''
+            INSERT INTO datasets
+            VALUES (?, ?, ?, ?, ?, ?)''', (
+            None,
+            formdata['type'],
+            formdata['title'],
+            formdata['description'],
+            formdata['license'],
+            formdata['path']
+            )
+        )
+        conn.commit()
+        datasetID = cur.lastrowid
+        for librisID in librisIDs:
+            cur.execute('''
+                INSERT INTO sameAs VALUES
+                (?, ?, ?)''', (
+                None, datasetID, librisID
+                )
+            )
+            conn.commit()
+        for format in encodingFormats:
+            cur.execute('''
+                INSERT INTO distribution
+                VALUES (?, ?, ?)''', (
+                None, datasetID, format
+                )
+            )
+            conn.commit()
+        cur.execute('''
+            INSERT INTO providor
+            VALUES (?, ?, ?, ?)''', (
+            None, datasetID, formdata['name'], formdata['email']
+            )
+        )
+        conn.commit()
+#    except Exception as e:
+#        print("Couldn't create dataset: %s" % e)
 
 
 def loadDatasets():
@@ -36,6 +141,49 @@ def loadDatasets():
 datasets, datasetPaths = loadDatasets()
 
 
+def loadDatasetsDB():
+    datasetList = []
+    cur = gconn.cursor()
+    cur.execute('''SELECT * from datasets''')
+    datasets = cur.fetchall()
+    for dataset in datasets:
+        print dataset
+        cur.execute(
+            '''
+            SELECT librisID from sameAs WHERE
+            datasetID = ?''', (dataset['datasetID'], )
+        )
+        dataset['sameAs'] = []
+        librisIDs = cur.fetchall()
+        for librisID in librisIDs:
+            dataset['sameAs'].append(librisID)
+        #cur.execute(
+        #    '''
+        #    SELECT format from distribution WHERE
+        #    datasetID = ?''', (dataset['datasetID'], )
+        #)
+        #formats = cur.fetchall()
+        formats = []
+        dataset['distribution'] = {}
+        dataset['distribution']['encodingFormat'] = []
+        for format in formats:
+            dataset['distribution']['encodingFormat'].append(format)
+        cur.execute(
+            '''
+            SELECT name, email FROM providor WHERE
+            datasetID = ?''', (dataset['datasetID'], )
+        )
+        providor = cur.fetchone()
+        dataset['providor'] = {
+            'name': providor['name'],
+            'email': providor['email']
+        }
+        datasetList.append(dataset)
+    return datasetList
+
+print loadDatasetsDB()
+
+
 @app.route('/')
 def index():
     print datasets
@@ -47,6 +195,14 @@ def index():
         )
     )
 
+
+@app.route('/new', methods=['GET', 'POST'])
+def addDataset():
+    if request.method == 'GET':
+        return(render_template('newform.html'))
+    if request.method == 'POST':
+        create_dataset(request.form)
+        return('Created!')
 
 if __name__ == "__main__":
     app.run(debug=True)
