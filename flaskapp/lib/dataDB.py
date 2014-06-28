dc084f1cc27f37a2764bfdcc820558ce60b2b486
Peter Krantz <peter@peterkrantz.se>
2014-06-28 13:38:21 +0200

    KB->kB + Kungliga->Kungl.

--- flaskapp/lib/dataDB.py
+++ flaskapp/lib/dataDB.py
@@ -19,7 +19,7 @@ def cleanDate(timestamp):
 
 
 def sizeof_fmt(num):
-    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+    for x in ['bytes', 'kB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
         num /= 1024.0
