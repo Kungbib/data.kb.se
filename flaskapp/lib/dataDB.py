f7d1db5e506ebf1006eabef29c1b65f2af2e27e6
sterutkb <stew.rutledge@gmail.com>
2015-08-21 11:53:25 +0200

    Moved index_dir to a class to make optional aspect easier to handle

--- flaskapp/lib/dataDB.py
+++ flaskapp/lib/dataDB.py
@@ -1,7 +1,6 @@
 from os import path, listdir
 from mimetypes import guess_type
 from urllib2 import quote
-from lxml import etree
 
 
 def cleanDate(timestamp):
@@ -16,66 +15,94 @@ def sizeof_fmt(num):
         num /= 1024.0
 
 
-def uploadSunet(torrentData, apiKey):
-    files = {'torrent': torrentData}
-    headers = {
-    'Accept': 'application/json',
-    'Authorization': 'Bearer %s' % apiKey
-    }
+class directory_indexer():
+    def __init__(self, getMets=False):
+        self.getMets = getMets
 
+    def summarizeMets(self, rawMeta):
+        from lxml import etree
+        mets = etree.XML(rawMeta)
+        mods = mets.xpath(
+            '/mets:mets/mets:dmdSec[@ID="dmdSec001"]//mods:mods',
+            namespaces=mets.nsmap
+        )[0]
+        related = mods.xpath(
+            'mods:relatedItem[@type="host"]',
+            namespaces=mods.nsmap
+        )[0]
+        title = related.xpath(
+            'mods:titleInfo/mods:title',
+            namespaces=mods.nsmap
+        )[0].text
+        issued = related.xpath(
+            'mods:part/mods:date',
+            namespaces=mods.nsmap
+        )[0].text
+        urn = mods.xpath(
+            'mods:identifier[@type="urn"]',
+            namespaces=mods.nsmap
+        )[0].text
+        languageBase = mods.xpath('mods:language', namespaces=mods.nsmap)
+        langList = []
+        for lang in languageBase:
+            langList.append(
+                lang.xpath('mods:languageTerm', namespaces=mods.nsmap)[0].text)
+        langs = ','.join(langList)
+        uri = related.xpath(
+            'mods:identifier[@type="uri"]',
+            namespaces=mods.nsmap
+        )[0].text
+        issn = related.xpath(
+            'mods:identifier[@type="issn"]',
+            namespaces=mods.nsmap
+        )[0].text
+        summary = {
+            'issn': issn,
+            'uri': uri,
+            'issued': issued,
+            'title': title,
+            'language': langs,
+            'urn': urn
+        }
+        return(summary)
 
-def summarizeMets(rawMeta):
-    mets = etree.XML(rawMeta)
-    mods = mets.xpath('/mets:mets/mets:dmdSec[@ID="dmdSec001"]//mods:mods', namespaces=mets.nsmap)[0]
-    related = mods.xpath('mods:relatedItem[@type="host"]', namespaces=mods.nsmap)[0]
-    title = related.xpath('mods:titleInfo/mods:title', namespaces=mods.nsmap)[0].text
-    issued = related.xpath('mods:part/mods:date', namespaces=mods.nsmap)[0].text
-    urn = mods.xpath('mods:identifier[@type="urn"]', namespaces=mods.nsmap)[0].text
-    languageBase = mods.xpath('mods:language', namespaces=mods.nsmap)
-    langList = []
-    for lang in languageBase:
-        langList.append(lang.xpath('mods:languageTerm', namespaces=mods.nsmap)[0].text)
-    langs = ','.join(langList)
-    uri = related.xpath('mods:identifier[@type="uri"]', namespaces=mods.nsmap)[0].text
-    issn = related.xpath('mods:identifier[@type="issn"]', namespaces=mods.nsmap)[0].text
-    return({'issn': issn, 'uri': uri, 'issued': issued, 'title': title, 'language': langs, 'urn': urn})
-
-
-def index_dir(directory, dataset, datasetRoot):
-    dTemp = directory
-    pathDict = {}
-    if directory is None and dataset.path != '':
-        directory = dataset.path
-    if directory != dataset.path:
-        directory = path.join(dataset.path, directory)
-    if directory is not None:
-        if directory != dataset.path and dTemp is not None:
-            dirUp = path.split(dTemp)[0]
-        if directory == dataset.path:
-            dirUp = None
-        if 'aip.mets.metadata' in listdir(path.join(datasetRoot, directory)):
-            print("Mets found")
-            with open(path.join(datasetRoot, directory, 'aip.mets.metadata')) as m:
-                metadata = summarizeMets(m.read())
-        else:
+    def index_dir(self, directory, dataset, datasetRoot):
+        dTemp = directory
+        pathDict = {}
+        if directory is None and dataset.path != '':
+            directory = dataset.path
+        if directory != dataset.path:
+            directory = path.join(dataset.path, directory)
+        if directory is not None:
+            if directory != dataset.path and dTemp is not None:
+                dirUp = path.split(dTemp)[0]
+            if directory == dataset.path:
+                dirUp = None
             metadata = None
-        for f in listdir(path.join(datasetRoot, directory)):
-            fullPath = path.join(datasetRoot, directory, f)
-            if path.isfile(fullPath):
-                pathDict[f] = {}
-                pathDict[f]['mimetype'] = guess_type(fullPath)[0]
-                pathDict[f]['name'] = f
-                pathDict[f]['realPath'] = path.join(
-                    datasetRoot,
-                    quote(directory),
-                    quote(f)
-                )
-                pathDict[f]['type'] = 'file'
-                pathDict[f]['size'] = sizeof_fmt(path.getsize(fullPath))
-            if path.isdir(fullPath):
-                pathDict[f] = {}
-                pathDict[f]['name'] = f
-                pathDict[f]['mimetype'] = '-'
-                pathDict[f]['type'] = 'folder-open'
-                pathDict[f]['size'] = '-'
-    return(pathDict, dirUp, metadata)
+            if self.getMets:
+                thisDir = path.join(datasetRoot, directory)
+                dirList = listdir(thisDir)
+                if 'aip.mets.metadata' in dirList:
+                    print("Mets found")
+                    with open(path.join(thisDir, 'aip.mets.metadata')) as m:
+                        metadata = self.summarizeMets(m.read())
+            for f in listdir(path.join(datasetRoot, directory)):
+                fullPath = path.join(datasetRoot, directory, f)
+                if path.isfile(fullPath):
+                    pathDict[f] = {}
+                    pathDict[f]['mimetype'] = guess_type(fullPath)[0]
+                    pathDict[f]['name'] = f
+                    pathDict[f]['realPath'] = path.join(
+                        datasetRoot,
+                        quote(directory),
+                        quote(f)
+                    )
+                    pathDict[f]['type'] = 'file'
+                    pathDict[f]['size'] = sizeof_fmt(path.getsize(fullPath))
+                if path.isdir(fullPath):
+                    pathDict[f] = {}
+                    pathDict[f]['name'] = f
+                    pathDict[f]['mimetype'] = '-'
+                    pathDict[f]['type'] = 'folder-open'
+                    pathDict[f]['size'] = '-'
+        return(pathDict, dirUp, metadata)
